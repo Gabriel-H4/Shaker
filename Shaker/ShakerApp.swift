b0866aeb2f94bbd87c9fa47fed1ca9258d4634f0
Gabriel Hassebrock <gabriel.h4@icloud.com>
2023-03-01 21:53:09 -0600

    Initial Commit

---
+++ Shaker/ShakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShakerApp.swift
+//  Shaker
+//
+//  Created by Gabriel Hassebrock on 3/1/23.
+//
+
+import SwiftUI
+
+@main
+struct ShakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
